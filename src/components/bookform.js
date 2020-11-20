c5144b06d89fd97330e0bd61eba18cf8e3098dd9
Michael Threels <michaelthreels@gmail.com>
2020-11-20 20:53:56 +0100

    feat: created bookform component and implemented form

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -1,7 +1,19 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
+const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
 const BookForm = () => (
-  <h1>bookform</h1>
+  <div className="inputContainer">
+    <form action="">
+      <label htmlFor="title">book title</label>
+      <input name="title" id="title" type="text" />
+      <label htmlFor="category">Category</label>
+      <select id="category">
+        {category.map(x => <option key>{x}</option>)}
+      </select>
+    </form>
+  </div>
 );
 
 export default BookForm;
