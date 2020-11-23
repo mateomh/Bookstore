3c691f692eaf6931b1016fb3b0068afa157311a6
Michael Threels <michaelthreels@gmail.com>
2020-11-23 20:36:08 +0100

    feat: implemented hooks in bookform

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -1,19 +1,27 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BookForm = () => (
-  <div className="inputContainer">
-    <form action="">
-      <label htmlFor="title">book title</label>
-      <input name="title" id="title" type="text" />
-      <label htmlFor="category">Category</label>
-      <select id="category">
-        {category.map(x => <option key>{x}</option>)}
-      </select>
-    </form>
-  </div>
-);
+const BookForm = () => {
+  const [title, setTitle] = useState();
+  const [category, setCategory] = useState();
+
+  const handleChange = () => {
+    
+  };
+  return (
+    <div className="inputContainer">
+      <form action="">
+        <label htmlFor="title">book title</label>
+        <input name="title" id="title" type="text" />
+        <label htmlFor="category">Category</label>
+        <select id="category">
+          {categories.map(x => <option key>{x}</option>)}
+        </select>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
