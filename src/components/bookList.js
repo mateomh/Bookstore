e1bcfbc5bea7c2ef2d77e3983d9240268d245a98
mateomh <mmojicah.84@gmail.com>
2020-11-23 16:29:01 -0500

    Add remove book functionality in the reducer and booklist component

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,13 +1,22 @@
 /* eslint-disable */
 
 import React from 'react';
-import { useStore } from 'react-redux';
+import { useStore, useDispatch } from 'react-redux';
 import Book from './book';
+import * as Actions from '../actions/index';
 
 const bookList = () => {
   // const books = useSelector(state => state.books);
   const store = useStore();
+  const dispatch = useDispatch();
   const { books } = store.getState();
+
+  const handleRemoveBook = (event) => {
+    const bookId = parseFloat(event.target.value);
+    const book = books.find(book => book.id === bookId);
+    dispatch(Actions.deleteBook(book));
+  };
+
   return (
     <table>
       <thead>
@@ -19,7 +28,7 @@ const bookList = () => {
       </thead>
       <tbody>
       {books.map(book => (
-        <Book key={book.id} book={book} />
+        <Book key={book.id} book={book} callbackFcn={handleRemoveBook} />
       ))}
       </tbody>
     </table>
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable no-unused-vars */
 const booksReducer = (state, action) => {
-  switch (action.type) {
+  const { type, payload: book } = action;
+  switch (type) {
     case 'CREATE_BOOK':
       return {
         ...state,
         books: state.books.concat(action.payload),
       };
     case 'REMOVE_BOOK':
-      return state;
+      const { id } = book;
+      const index = state.books.findIndex(book => book.id === id);
+      const newBooks = [...state.books];
+      newBooks.splice(index, 1);
+      const newstate = {
+        ...state,
+        books: [...newBooks],
+      };
+      console.log('Reducer');
+      console.log(index);
+      return newstate;
     default:
       return state;
   }
