6b96a91f3485be3455b1e1cb7aeba83efbdd4e48
mateomh <mmojicah.84@gmail.com>
2020-11-23 17:44:05 -0500

    Add re-rendering function to index.js for updating the app

--- src/index.js
+++ src/index.js
@@ -28,9 +28,15 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+const testRender = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
+
+store.subscribe(testRender);
+
+testRender();
